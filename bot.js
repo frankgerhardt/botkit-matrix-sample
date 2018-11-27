da2c81f05b4ed6004a6cc0967830cdca4c201423
Lukács Sándor <sanya@sanya>
2018-11-27 16:33:07 +0100

    BOTS-12 uploading the first version of botkit-matrix-sample

---
+++ bot.js
@@ -0,0 +1,18 @@
+let config = {
+    'baseUrl': 'https://matrix.org',
+    'botUserId': '@gibot3:matrix.org',
+    'password': 'chatbot2018!',
+    'localStorage': './storage'
+};
+
+require('botkit-matrix').MatrixController(config)
+    .then((controller) => {
+
+        controller.hears(['hi', 'hello'], 'message_received', function (bot, message) {
+            bot.reply(message, "Hello, world!");
+        });
+
+        controller.hears(['.*'], 'message_received', function (bot, message) {
+            bot.reply(message, "Sorry, I can't understand. :(");
+        });
+    });
